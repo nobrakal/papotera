4a3b2e6aae29243742d5e7d2c64dc3dac8e5cd60
Alexandre Moine <alexandre@moine.me>
2020-06-08 11:51:11 +0200

    cleanup

--- src/ES.v
+++ src/ES.v
@@ -7,39 +7,40 @@ Require Import Causality.Utils.
 Definition irreflexive A R: Prop := forall x:A, not (R x x).
 
 (** Conflict relation *)
-Record conflict t c := { conflict_sym : symmetric t c;
-                         conflict_irfl : irreflexive t c}.
+Record conflict A R := { conflict_sym : symmetric A R;
+                         conflict_irfl : irreflexive A R}.
 
-Definition conflict_inherit {t} cmp cfl :=
-  forall x y z:t, cfl x y -> cmp y z -> cfl x z.
+Definition conflict_inherit {A} (cmp cfl : relation A) :=
+  forall x y z, cfl x y -> cmp y z -> cfl x z.
 
 Set Implicit Arguments.
 
-Record ES s t :=
-  mkES { cmp : relation t; cmp_ord : order t cmp;
-         cfl : relation t; cfl_conflict : conflict t cfl;
+Record ES (Lbl : Set) :=
+  mkES { Event: Set;
+         cmp : relation Event; cmp_ord : order Event cmp;
+         cfl : relation Event; cfl_conflict : conflict Event cfl;
          inherit : conflict_inherit cmp cfl;
-         lbl : t -> s}.
+         lbl : Event -> Lbl}.
 
-Definition par_rel a b (ra : relation a) (rb : relation b) : relation (a + b) :=
+Definition par_rel A B (ra : relation A) (rb : relation B) : relation (A + B) :=
   fun x y =>
     match x,y with
     | inl x, inl y => ra x y
     | inr x, inr y => rb x y
     | _,_ => False end.
 
-Lemma par_order a b (ra : relation a) (ra_o:order a ra) (rb : relation b) (rb_o:order b rb) :
-  order (a + b) (par_rel ra rb).
+Lemma par_order A B (ra : relation A) (ra_o:order A ra) (rb : relation B) (rb_o:order B rb) :
+  order (A + B) (par_rel ra rb).
 Proof.
   split.
-  - intro x; case x; firstorder.
+  - intros x; case x; firstorder.
   - intros x y z rxy ryz; case x,y,z; firstorder.
   - intros x y rxy ryx.
     case x,y; simpl in *; try easy; f_equal; firstorder.
 Qed.
 
-Lemma par_conflict a b (ra : relation a) (ra_o:conflict a ra) (rb : relation b) (rb_o:conflict b rb) :
-  conflict (a + b) (par_rel ra rb).
+Lemma par_conflict A B (ra : relation A) (ra_o:conflict A ra) (rb : relation B) (rb_o:conflict B rb) :
+  conflict (A + B) (par_rel ra rb).
 Proof.
   split.
   - intros x y rxy; case x,y; firstorder.
@@ -54,7 +55,7 @@ Proof.
 Qed.
 
 (* Parallel composition of two ES *)
-Definition par_es S A B (E:ES S A) (F:ES S B) :=
+Definition par_es Lbl (E:ES Lbl) (F:ES Lbl) :=
   let cmp := par_rel E.(cmp) F.(cmp) in
   let cmp_order := par_order  E.(cmp_ord) F.(cmp_ord) in
   let cfl := par_rel E.(cfl) F.(cfl) in
@@ -62,36 +63,6 @@ Definition par_es S A B (E:ES S A) (F:ES S B) :=
   let inherit := par_inherit  E.(inherit)  F.(inherit) in
   let lbl := either E.(lbl) F.(lbl) in
   mkES cmp_order cfl_conflict inherit lbl.
-
-(* Arbitrary sum *)
-Definition ASum (A:Type) : nat -> Type :=
-  fix vec n :=
-    match n return Type with
-    | O   => False
-    | S n => sum A (vec n)
-    end.
-
-Definition empty_rel A : A -> A -> Prop := fun _ _ => False.
-
-Lemma empty_order : order _ (@empty_rel False).
-Proof. split; intuition; intros x; intuition. Qed.
-
-Lemma empty_conflict : conflict _ (@empty_rel False).
-Proof. split; intuition; intros x hx; intuition. Qed.
-
-Lemma empty_inherit : conflict_inherit (@empty_rel False) (@empty_rel False).
-Proof. firstorder. Qed.
-
-Lemma false_all S : False -> S . Proof. intuition. Qed.
-
-Definition empty_es S : ES S False :=
-  mkES empty_order empty_conflict empty_inherit (@false_all S).
-
-Fixpoint par_multiple_es S A n (xs:Vector.t (ES S A) n) : ES S (ASum A n) :=
-  match xs in Vector.t _ n return ES S (ASum A n) with
-  | Vector.nil _ => empty_es _
-  | Vector.cons _ x n xs =>
-    par_es x (par_multiple_es xs) end.
 
 Definition lift_rel A (R:relation A) : relation (option A) :=
   maybe (fun _ => False) (fun x => maybe False (R x)).
@@ -124,7 +95,7 @@ Proof.
 Qed.
 
 (* Prefixing for ES *)
-Definition prefixing_es S A (a:S) (E:ES S A) : ES S (option A) :=
+Definition prefixing_es Lbl (E:ES Lbl) (a:Lbl) : ES Lbl :=
   let cmp := prefix_opt E.(cmp) in
   let cmp_order := prefix_opt_order E.(cmp_ord) in
   let cfl := lift_rel E.(cfl) in
@@ -141,16 +112,16 @@ Definition downclosed a (cmp : relation a) (X: Ensemble a) :=
 Definition conflict_free a (cfl : relation a) (X: Ensemble a) :=
   forall x y, In _ X x -> In _ X y -> not (cfl x y).
 
-Definition Configuration S A (E: ES S A) x := downclosed E.(cmp) x /\ conflict_free E.(cfl) x.
+Definition Configuration (S:Set) (E: ES S) x := downclosed E.(cmp) x /\ conflict_free E.(cfl) x.
 
-Lemma empty S A (E:ES S A) : sig (Configuration E).
+Lemma empty (Lbl:Set) (E:ES Lbl) : sig (Configuration E).
 Proof.
   split with (x:=Empty_set _).
   split; [intros x ix | intros x y ix];
     now apply Noone_in_empty in ix.
 Defined.
 
-Definition lts_of_es S A (E:ES S A) : LTS S (sig (Configuration E)) :=
+Definition lts_of_es (Lbl:Set) (E:ES Lbl) : LTS Lbl :=
   let t x :=
       let x := proj1_sig x in
       fun '(c,a) =>
@@ -161,7 +132,7 @@ Definition lts_of_es S A (E:ES S A) : LTS S (sig (Configuration E)) :=
 (* ProofIrrelevance will be used to prove the equality of two sig negleting the equality of the proof. *)
 Require Coq.Logic.ProofIrrelevance.
 
-Lemma specif_eq' {A} (P:A -> Prop) (x y : A) (px: P x) (py: P y) : x = y -> exist P x px = exist P y py.
+Lemma specif_eq' A (P:A -> Prop) (x y : A) (px: P x) (py: P y) : x = y -> exist P x px = exist P y py.
 Proof.
   intros exy.
   apply eq_sig_hprop.
@@ -174,8 +145,9 @@ Proof. destruct x as (x,hx), y as (y,hy); apply specif_eq'. Qed.
 
 Section ParBisim.
 
-  Lemma union_ens S A B (E:ES S A) (F:ES S B) (P : {x : Ensemble A | Configuration E x} * {x : Ensemble B | Configuration F x}) :
-    {x:Ensemble (A + B) | Configuration (par_es E F) x}.
+  Variables (Lbl: Set) (E F:ES Lbl).
+
+  Lemma union_ens (P : sig (Configuration E) * sig (Configuration F)) : sig (Configuration (par_es E F)).
   Proof.
     split with (x:=either (fun x => In _ (proj1_sig (fst P)) x) (fun y => In _ (proj1_sig (snd P)) y)).
     destruct P as (X,Y).
@@ -187,20 +159,19 @@ Section ParBisim.
       destruct x,y; firstorder.
   Defined.
 
-  Lemma split_ens {S A B} (E:ES S A) (F:ES S B) (X:{x:Ensemble (A + B) | Configuration (par_es E F) x}):
-    {x : Ensemble A | Configuration E x}*{x : Ensemble B | Configuration F x}.
+  Lemma split_ens (X:sig (Configuration (par_es E F))) : sig (Configuration E) * sig (Configuration F).
   Proof.
     split;
       [split with (x:=left_part (proj1_sig X)) | split with (x:=right_part (proj1_sig X)) ];
       destruct X as (X,HX); simpl in *; firstorder.
   Defined.
 
-  Lemma union_split S A B (E:ES S A) (F:ES S B) X : @union_ens _ _ _ E F (split_ens X) = X.
+  Lemma union_split (X:sig (Configuration (par_es E F))) : union_ens (split_ens X) = X.
     apply specif_eq, Extensionality_Ensembles.
     split; intros x; destruct x; intuition.
   Qed.
 
-  Lemma split_union S A B (E:ES S A) (F:ES S B) X : @split_ens _ _ _ E F (union_ens X) = X.
+  Lemma split_union (X : sig (Configuration E) * sig (Configuration F)) : split_ens (union_ens X) = X.
     destruct X as ((X1,HX1),(X2,HX2)).
     unfold split_ens;
       apply f_equal2;
@@ -208,31 +179,31 @@ Section ParBisim.
       split; intuition.
   Qed.
 
-  Lemma par_Configuration S A B (E:ES S A) (F:ES S B) X :
+  Lemma par_Configuration X :
     Configuration (par_es E F) X -> Configuration E (left_part X) /\ Configuration F (right_part X).
   Proof. firstorder. Qed.
 
-  Lemma Configuration_either_l S A B (E:ES S A) (F:ES S B) X Y e:
-    Configuration E (Add A X e) -> Configuration F Y ->
+  Lemma Configuration_either_l X Y e:
+    Configuration E (Add _ X e) -> Configuration F Y ->
     Configuration (par_es E F)
-          (Add (A + B) (disjoint_union X Y) (inl e)).
+          (Add _ (disjoint_union X Y) (inl e)).
   Proof.
     destruct (add_either X Y) as (HL,HR).
     rewrite HL; intros ce cf.
     split; [intros x hx y| intros x y]; destruct x,y; firstorder.
   Qed.
 
-  Lemma Configuration_either_r S A B (E:ES S A) (F:ES S B) X Y e:
-    Configuration E X -> Configuration F (Add B Y e) ->
+  Lemma Configuration_either_r X Y e:
+    Configuration E X -> Configuration F (Add _ Y e) ->
     Configuration (par_es E F)
-          (Add (A + B) (disjoint_union X Y) (inr e)).
+          (Add _ (disjoint_union X Y) (inr e)).
   Proof.
     destruct (add_either X Y) as (HL,HR).
     rewrite HR; intros ce cf.
     split; [intros x hx y| intros x y]; destruct x,y; firstorder.
   Qed.
 
-  Lemma par_sim1 {S A B} (E:ES S A) (F:ES S B) :
+  Lemma par_sim1 :
     Simulation (par_lts (lts_of_es E) (lts_of_es F)) (lts_of_es (par_es E F))
                (fun x y => y=union_ens x).
   Proof.
@@ -254,7 +225,7 @@ Section ParBisim.
       + now apply Configuration_either_r.
   Qed.
 
-  Lemma par_sim2 {S A B} (E:ES S A) (F:ES S B) :
+  Lemma par_sim2 :
     Simulation (lts_of_es (par_es E F)) (par_lts (lts_of_es E) (lts_of_es F))
                (fun y x => y=union_ens x).
   Proof.
@@ -264,23 +235,23 @@ Section ParBisim.
     rewrite union_split.
     destruct tqq' as (e,He).
     split; intuition.
-    apply (f_equal (@split_ens _ _ _ E F)) in rqp; rewrite split_union in rqp.
+    apply (f_equal split_ens) in rqp; rewrite split_union in rqp.
     injection rqp as eqp'1 eqp'2; destruct eqp'1, eqp'2.
     apply eihter_set in H; destruct H as (Hl,Hr).
     destruct e.
     - left; split; simpl.
       + apply specif_eq; simpl.
         now rewrite wrong_add2 in Hr.
-      + exists a0; rewrite add_left; intuition; simpl.
+      + exists e; rewrite add_left; intuition; simpl.
         now destruct (par_Configuration H1).
     - right; split; simpl.
       + apply specif_eq; simpl.
         now rewrite wrong_add1 in Hl.
-      + exists b; rewrite add_right; intuition; simpl.
+      + exists e; rewrite add_right; intuition; simpl.
         now destruct (par_Configuration H1).
   Qed.
 
-  Theorem par_bisim S A B (E:ES S A) (F:ES S B) :
+  Theorem par_bisim :
     Bisimilar (par_lts (lts_of_es E) (lts_of_es F)) (lts_of_es (par_es E F)).
   Proof.
     exists (fun x y => y=union_ens x).
@@ -303,26 +274,26 @@ Proof. firstorder. Qed.
 
 Section PrefixingBisim.
 
-  Definition add_none_sig S A (E:ES S A) (a:S)  (x:{x : Ensemble A | Configuration E x}) : {x : Ensemble (option A) | Configuration (prefixing_es a E) x}.
-    split with (x:=add_none (proj1_sig x)).
+  Variables (Lbl: Set) (E:ES Lbl) (a:Lbl).
+
+  Definition add_none_sig (X:sig (Configuration E)) : sig (Configuration (prefixing_es E a)).
+    split with (x:=add_none (proj1_sig X)).
     split; [intros y iy z | intros y z iy];
-      destruct x,y,z; unfold In,maybe in *; simpl in *; firstorder.
+      destruct X,y,z; unfold In,maybe in *; simpl in *; firstorder.
   Defined.
 
-  Definition remove_none_sig S A (E:ES S A) (a:S) (X:{x : Ensemble (option A) | Configuration (prefixing_es a E) x}) : {x : Ensemble A | Configuration E x}.
+  Definition remove_none_sig (X:sig (Configuration (prefixing_es E a))) : sig (Configuration E).
     split with (x:=fun x => proj1_sig X (Some x)).
     destruct X as (X,HX).
     split; [intros y iy z | intros y z iy];
       unfold In,maybe in *; simpl in *; firstorder.
   Defined.
 
-  Lemma remove_add_none S A (a:S) (E:ES S A) (X:{x : Ensemble A | Configuration E x}) :
-    remove_none_sig (add_none_sig a X) = X.
+  Lemma remove_add_none (X:sig (Configuration E)) : remove_none_sig (add_none_sig X) = X.
   Proof. now apply specif_eq. Qed.
 
-  Lemma add_remove_none S A (a:S) (E:ES S A)
-        (X:{x : Ensemble (option A) | Configuration (prefixing_es a E) x})
-        (H:Inhabited _ (proj1_sig X)) : add_none_sig a (remove_none_sig X) = X.
+  Lemma add_remove_none (X:sig (Configuration (prefixing_es E a))) (H:Inhabited _ (proj1_sig X)) :
+    add_none_sig (remove_none_sig X) = X.
   Proof.
     apply specif_eq, Extensionality_Ensembles; simpl.
     split; intros x ix; destruct x; unfold In in *; simpl in *; intuition.
@@ -332,11 +303,11 @@ Section PrefixingBisim.
     specialize HX1 with x None; firstorder.
   Qed.
 
-  Definition therel S A (E:ES S A) (a:S) :
-    option {x : Ensemble A | Configuration E x} -> {x : Ensemble (option A) | Configuration (prefixing_es a E) x} -> Prop :=
-    fun x y => maybe (proj1_sig y=Empty_set _) (fun x => y = add_none_sig a x) x.
+  Definition therel : option (sig (Configuration E)) -> sig (Configuration (prefixing_es E a)) -> Prop :=
+    fun x y => maybe (proj1_sig y=Empty_set _) (fun x => y = add_none_sig x) x.
 
-  Lemma Configuration_add_none S A (E:ES S A) (a:S) X : Configuration E X -> Configuration (prefixing_es a E) (add_none X).
+  Lemma Configuration_add_none X :
+    Configuration E X -> Configuration (prefixing_es E a) (add_none X).
   Proof.
     intros H.
     split.
@@ -344,21 +315,21 @@ Section PrefixingBisim.
     - intros x y R cxy; destruct x,y; firstorder.
   Qed.
 
-  Lemma pref_sim1 S A (E:ES S A) (a:S) :
-    Simulation (prefixing_lts a (lts_of_es E)) (lts_of_es (prefixing_es a E)) (@therel _ _ E a).
+  Lemma pref_sim1 :
+    Simulation (prefixing_lts (lts_of_es E) a) (lts_of_es (prefixing_es E a)) therel.
   Proof.
     intros p q rpq p' b tpp'.
     destruct p' as [p'|]; simpl in *.
     - destruct p as [p|]; simpl in *.
-      + exists (add_none_sig a p'); simpl in *; intuition.
+      + exists (add_none_sig p'); simpl in *; intuition.
         destruct tpp' as (e,(H1,(H2,(H3,H4)))).
         rewrite rpq; simpl.
         exists (Some e); simpl; rewrite add_none_add_eq; intuition.
         * now apply f_equal.
         * now apply Configuration_add_none.
-      + exists (add_none_sig a (empty _)); simpl in *; intuition.
+      + exists (add_none_sig (empty _)); simpl in *; intuition.
         * destruct q as (q,Hq).
-          assert (add_none (Empty_set A) = Add (option A) q None).
+          assert (add_none (Empty_set _) = Add (option _) q None).
           -- apply Extensionality_Ensembles; split; intros x ix; destruct x; unfold In in *; simpl in *;
                firstorder using Add_intro2.
              apply Add_inv in ix; destruct ix; simpl; try congruence.
@@ -379,8 +350,8 @@ Section PrefixingBisim.
     - injection H as h; rewrite h; apply Add_intro2.
   Qed.
 
-  Lemma Configuration_add S A (E:ES S A) (a:S) q e :
-    Configuration (prefixing_es a E) (Add (option A) q (Some e)) -> Configuration E (Add A (fun x : A => q (Some x)) e).
+  Lemma Configuration_add q e :
+    Configuration (prefixing_es E a) (Add _ q (Some e)) -> Configuration E (Add _ (fun x  => q (Some x)) e).
   Proof.
     intros (C1,C2).
     split.
@@ -390,7 +361,7 @@ Section PrefixingBisim.
       apply Add_inv in ix; destruct ix.
       + apply Add_intro1 with (x:=e), Add_opt,C1,Add_inv in H; intuition.
         injection H0 as h; rewrite h; intuition.
-      + assert (In (option A) (Add (option A) q (Some e)) (Some x)).
+      + assert (In _ (Add _ q (Some e)) (Some x)).
         rewrite H; intuition.
         apply C1,Add_inv in H0; intuition.
         injection H1 as h; rewrite h; intuition.
@@ -404,8 +375,8 @@ Section PrefixingBisim.
         rewrite H0; intuition.
   Qed.
 
-  Lemma pref_sim2 S A (E:ES S A) (a:S) :
-    Simulation (lts_of_es (prefixing_es a E)) (prefixing_lts a (lts_of_es E)) (fun x y => @therel _ _ E a y x).
+  Lemma pref_sim2 :
+    Simulation (lts_of_es (prefixing_es E a)) (prefixing_lts (lts_of_es E) a) (fun x y => therel y x).
   Proof.
     intros q p rqp q' b tqq'.
     destruct tqq' as (e,(H1,(H2,(H3,H4)))).
@@ -413,17 +384,17 @@ Section PrefixingBisim.
     destruct p as [p|]; simpl in *.
     - destruct e as [e|].
       + exists (Some (remove_none_sig q')); simpl in *.
-        destruct q as (q,Hq); apply (f_equal (@remove_none_sig _ _ E a)),proj1_sig_eq in rqp; simpl in *.
+        destruct q as (q,Hq); apply (f_equal remove_none_sig),proj1_sig_eq in rqp; simpl in *.
         destruct p as (p,Hp); simpl in *.
         split.
-        * assert ((Add A p e) = Add A (fun x => p x) e) by intuition.
+        * assert ((Add _ p e) = Add _ (fun x => p x) e) by intuition.
           exists e; rewrite H, <- rqp; intuition.
           -- apply Extensionality_Ensembles; split; intros x ix.
              ++ apply H1,Add_inv in ix; destruct ix; intuition.
                 injection H0 as h; rewrite h; intuition.
              ++ apply H1; apply Add_inv in ix; destruct ix; intuition.
                 rewrite H0; intuition.
-          -- now apply Configuration_add with a.
+          -- now apply Configuration_add.
           -- apply H3; apply Extension in rqp; now apply rqp.
         * assert (Inhabited _ (proj1_sig q')).
           -- apply (Inhabited_intro _ _ (Some e)).
@@ -438,18 +409,18 @@ Section PrefixingBisim.
       apply Extension in rqp; destruct rqp as (R1,R2).
       destruct e.
       + exfalso.
-        assert (~ (In _ (Add _ (proj1_sig q) (Some a0)) None)).
+        assert (~ (In _ (Add _ (proj1_sig q) (Some e)) None)).
         * unfold not; intros H.
           apply Add_inv in H; destruct H; try congruence; firstorder.
         * apply H.
           destruct H2 as (H21,H22).
           unfold downclosed,cmp,prefixing_es in H21.
-          specialize H21 with (Some a0) None.
+          specialize H21 with (Some e) None.
           apply H21; simpl; intuition.
       + exists (Some (empty _)); simpl in *; intuition.
         apply specif_eq,Extensionality_Ensembles; simpl.
         symmetry in H1.
-        transitivity (Add (option A) (proj1_sig q) None); intuition.
+        transitivity (Add _ (proj1_sig q) None); intuition.
         split; intros x ix; destruct x; unfold In in *; simpl in *; intuition.
         * apply Add_inv in ix.
           destruct ix; try congruence.
@@ -457,10 +428,10 @@ Section PrefixingBisim.
         * apply Add_intro2.
   Qed.
 
-  Theorem prefixing_bisim S A (E:ES S A) (a:S) :
-    Bisimilar (prefixing_lts a (lts_of_es E)) (lts_of_es (prefixing_es a E)).
+  Theorem prefixing_bisim :
+    Bisimilar (prefixing_lts (lts_of_es E) a) (lts_of_es (prefixing_es E a)).
   Proof.
-    exists (@therel _ _ E a).
+    exists therel.
     split; try split.
     - apply pref_sim1.
     - apply pref_sim2.
--- src/LTS.v
+++ src/LTS.v
@@ -5,11 +5,12 @@ Require Import Causality.Utils.
 
 Set Implicit Arguments.
 
-Record LTS s x :=
-  mkLTS { trans : x -> Ensemble (x * s);
-          start : x }.
+Record LTS (Lbl:Set) :=
+  mkLTS { State : Type;
+          trans : State -> Ensemble (State * Lbl);
+          start : State }.
 
-Definition prefixing_lts S A (a:S) (X:LTS S A) : LTS S (option A) :=
+Definition prefixing_lts S (X:LTS S) (a:S) : LTS S :=
   let trans x :=
       fun '(x',a') =>
         match x with
@@ -17,38 +18,21 @@ Definition prefixing_lts S A (a:S) (X:LTS S A) : LTS S (option A) :=
         | Some x => maybe False (fun x' => X.(trans) x (x',a')) x' end in
   mkLTS trans None.
 
-Definition par_lts S A B (X:LTS S A) (Y:LTS S B) :=
+Definition par_lts S (X:LTS S) (Y:LTS S) :=
   let trans '(x,y) :=
       (fun '((x',y'),a) =>
          (y=y' /\ In _ (X.(trans) x) (x',a)) \/ (x=x' /\ In _ (Y.(trans) y) (y',a))) in
   let start := (X.(start), Y.(start)) in
   mkLTS trans start.
 
-(* Arbitrary tuple *)
-Definition ATuple (A:Type) : nat -> Type :=
-  fix vec n :=
-    match n return Type with
-    | O   => unit
-    | S n => prod A (vec n)
-    end.
+Definition Trans S (X:LTS S) p p' a : Prop := In _ (X.(trans) p) (p',a).
 
-Definition unit_lts S :=
-  @mkLTS S unit (fun _ => Empty_set _) tt.
-
-Fixpoint par_multiple_lts S A n (xs:Vector.t (LTS S A) n) : LTS S (ATuple A n) :=
-  match xs in Vector.t _ n return LTS S (ATuple A n) with
-  | Vector.nil _ => unit_lts _
-  | Vector.cons _ x n xs =>
-    par_lts x (par_multiple_lts xs) end.
-
-Definition Trans S A (X:LTS S A) (p p':A) (a:S) : Prop := In _ (X.(trans) p) (p',a).
-
-Definition Simulation S A B (X:LTS S A) (Y:LTS S B) (R :A -> B -> Prop) :=
+Definition Simulation S (X:LTS S) (Y:LTS S) (R: X.(State) -> Y.(State) -> Prop) :=
   forall p q, R p q ->
   forall p' a, Trans X p p' a -> exists q', Trans Y q q' a /\ R p' q'.
 
-Definition Bisimulation S A B (X:LTS S A) (Y:LTS S B) (R:A->B->Prop) : Prop :=
+Definition Bisimulation S (X:LTS S) (Y:LTS S) (R: X.(State) -> Y.(State) -> Prop) : Prop :=
   Simulation X Y R /\ Simulation Y X (fun x y => R y x) /\ R X.(start) Y.(start).
 
-Definition Bisimilar S A B (X:LTS S A) (Y:LTS S B) : Prop :=
+Definition Bisimilar S (X:LTS S) (Y:LTS S) : Prop :=
   exists R, Bisimulation X Y R.
